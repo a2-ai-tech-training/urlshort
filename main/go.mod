9d94d3586412b04eb5dcdee64b1d6241356e2d79
Kelley Ballentine <alt4kcb@gmail.com>
2024-03-10 18:15:57 -0400

    feat: go.mod

---
+++ main/go.mod
@@ -0,0 +1,3 @@
+module github.com/a2-ai-tech-training/urlshort
+
+go 1.21.6
--- main/main.go
+++ main/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gophercises/urlshort"
+	"github.com/a2-ai-tech-training/urlshort"
 )
 
 func main() {
